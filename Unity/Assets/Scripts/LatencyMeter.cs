2feb8d909493d7120ecf89700383e3dc21eab9f0
Sebastian Friston <sebastian.friston@gmail.com>
2021-07-09 20:18:32 +0100

    Added new measurement components and scenes to facilitate data capture for VRST21

---
+++ Unity/Assets/Scripts/LatencyMeter.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Ubiq.Logging;
+using Ubiq.Messaging;
+using Ubiq.Rooms;
+using UnityEngine;
+
+public class LatencyMeter : MonoBehaviour, INetworkComponent
+{
+    private NetworkContext context;
+    private RoomClient client;
+    private Dictionary<string, Stopwatch> transmissionTimes;
+    private EventLogger latencies;
+    private float lastMeasureTime;
+
+    public float MeasurePeriod;
+
+    private void Awake()
+    {
+        transmissionTimes = new Dictionary<string, Stopwatch>();
+        lastMeasureTime = 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        context = NetworkScene.Register(this);
+        client = context.scene.GetComponentInChildren<RoomClient>();
+        latencies = new UserEventLogger(this);
+        client.OnRoom.AddListener(OnRoom);
+    }
+
+    void OnRoom(RoomInfo room)
+    {
+        float measurePeriod;
+        if(float.TryParse(room["exp.latencymeter.period"], out measurePeriod))
+        {
+            MeasurePeriod = measurePeriod;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(MeasurePeriod > 0)
+        {
+            var measureInterval = Time.time - lastMeasureTime;
+            if(measureInterval > MeasurePeriod)
+            {
+                lastMeasureTime = Time.time;
+                MeasurePeerLatencies();
+            }
+        }
+    }
+
+    private struct Message
+    {
+        public NetworkId source;
+        public string token;
+        public bool reply;
+    }
+
+    public void MeasurePeerLatencies()
+    {
+        foreach (var item in client.Peers)
+        {
+            if(item.UUID == client.Me.UUID)
+            {
+                continue;
+            }
+
+            Message message;
+            message.source = context.scene.Id;
+            message.token = Guid.NewGuid().ToString();
+            message.reply = false;
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            transmissionTimes[message.token] = stopwatch;
+            context.SendJson(item.NetworkObjectId, message);
+            latencies.Log("RTTA", context.scene.Id, item.NetworkObjectId, message.token);
+        }
+    }
+
+    public void ProcessMessage(ReferenceCountedSceneGraphMessage message)
+    {
+        var msg = message.FromJson<Message>();
+        var source = msg.source;
+
+        if (msg.reply)
+        {
+            latencies.Log("RTTC", context.scene.Id, msg.token);
+            Stopwatch stopwatch;
+            if(transmissionTimes.TryGetValue(msg.token, out stopwatch))
+            {
+                stopwatch.Stop();
+                latencies.Log("RTTM", context.scene.Id, source, stopwatch.ElapsedMilliseconds);
+            }
+            transmissionTimes.Remove(msg.token);
+        }
+        else
+        {
+            latencies.Log("RTTB", context.scene.Id, msg.token);
+            msg.source = context.scene.Id;
+            msg.reply = true;
+            context.SendJson(source, msg);
+        }
+    }
+
+    public void SetPeriod(float period)
+    {
+        MeasurePeriod = period;
+        client.Room["exp.latencymeter.period"] = period.ToString();
+    }
+}
---
+++ Unity/Assets/Scripts/ThroughputMonitor.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Ubiq.Messaging;
+using Ubiq.Networking;
+using Ubiq.Voip;
+using UnityEngine;
+
+public class ThroughputMonitor : MonoBehaviour
+{
+    private NetworkScene scene;
+    private Ubiq.Networking.JmBucknall.Structures.LockFreeQueue<MessageEvent> queue;
+    private StreamWriter stream;
+    private Task task;
+    private bool run;
+    private float timeNow;
+
+    private void Awake()
+    {
+        queue = new Ubiq.Networking.JmBucknall.Structures.LockFreeQueue<MessageEvent>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scene = NetworkScene.FindNetworkScene(this);
+        scene.OnMessageSent.AddListener(Sent);
+        scene.OnMessageReceived.AddListener(Received);
+        task = new Task(Worker);
+        run = true;
+        stream = new StreamWriter(OpenNewFile());
+        task.Start();
+
+        G722AudioDecoder.OnDecode = OnDecode;
+        G722AudioEncoder.OnEncode = OnEncode;
+    }
+
+    void Update()
+    {
+        timeNow = Time.realtimeSinceStartup;
+    }
+
+    private void OnDecode(byte[] encoded)
+    {
+        MessageEvent e;
+        e.time = timeNow;
+        e.length = encoded.Length;
+        e.direction = false;
+        e.id = "0";
+        queue.Enqueue(e);
+    }
+
+    private void OnEncode(byte[] encoded)
+    {
+        MessageEvent e;
+        e.time = timeNow;
+        e.length = encoded.Length;
+        e.direction = true;
+        e.id = "0";
+        queue.Enqueue(e);
+    }
+
+    public struct MessageEvent
+    {
+        public float time;
+        public int length;
+        public bool direction;
+        public string id;
+    }
+
+    void Sent(ReferenceCountedMessage message)
+    {
+        MessageEvent e;
+        e.time = Time.realtimeSinceStartup;
+        e.length = message.length;
+        e.direction = true;
+        e.id = new ReferenceCountedSceneGraphMessage(message).objectid.ToString();
+        queue.Enqueue(e);
+    }
+    
+    void Received(ReferenceCountedMessage message)
+    {
+        MessageEvent e;
+        e.time = Time.realtimeSinceStartup;
+        e.length = message.length;
+        e.direction = false;
+        e.id = new ReferenceCountedSceneGraphMessage(message).objectid.ToString();
+        queue.Enqueue(e);
+    }
+
+    void Worker()
+    {        
+        while(run)
+        {
+            MessageEvent e;
+            if(queue.Dequeue(out e))
+            {
+                try
+                {
+                    stream.WriteLine($"{e.time}, {e.length}, {e.direction}, {e.id}");
+                }
+                catch(Exception)
+                {
+                    return;
+                }
+            }
+        }
+    }
+
+    Stream OpenNewFile()
+    {
+        int i = 0;
+        string filename = Filepath(i);
+        while (File.Exists(filename))
+        {
+            filename = Filepath(i++);
+        }
+
+        return File.Open(filename, FileMode.Create, FileAccess.Write, FileShare.Read);
+    }
+
+    private string Filepath(int i)
+    {
+        return Path.Combine(Application.persistentDataPath, $"UbiqThroughputMonitor_log_{DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss")}_{i}.csv");
+    }
+
+    private void OnDestroy()
+    {
+        run = false;
+        stream.Close();
+        task.Wait();
+    }
+
+}
