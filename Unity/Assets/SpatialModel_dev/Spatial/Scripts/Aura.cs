d65a46f3e1114aa284ff8861e06cae88b132f71e
zy967 <ys790094203@gmail.com>
2022-08-21 20:13:33 +0100

    Initial spatial model scripts

---
+++ Unity/Assets/SpatialModel_dev/Spatial/Scripts/Aura.cs
@@ -0,0 +1,14 @@
+using Ubiq.Rooms;
+using UnityEngine;
+
+namespace SpatialModel
+{
+/**
+* A fixed area host a room
+*/
+public class Aura : MonoBehaviour
+{
+	private RoomClient roomClient;
+}
+
+}
